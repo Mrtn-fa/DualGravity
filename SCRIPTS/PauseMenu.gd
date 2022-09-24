1296cc0909d7b4a2a1290504f0399a71a4fcfaeb
Mrtnfa <mnfaray@gmail.com>
2022-09-24 19:36:28 -0300

    mainMenu

--- SCRIPTS/PauseMenu.gd
+++ SCRIPTS/PauseMenu.gd
@@ -1,16 +1,20 @@
 extends Control
 
+onready var play = $VBoxContainer/play
+onready var credits = $VBoxContainer/credits
+onready var exit = $VBoxContainer/exit
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	play.connect("pressed", self, "_on_play_pressed")
+	credits.connect("pressed", self, "_on_credits_pressed")
+	exit.connect("pressed", self, "_on_exit_pressed")
 
+func _on_play_pressed():
+	get_tree().change_scene("res://LEVELS/LEVEL_1.tscn")
+	
+func _on_credits_pressed():
+	print("Credits")
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_exit_pressed():
+	get_tree().quit()
